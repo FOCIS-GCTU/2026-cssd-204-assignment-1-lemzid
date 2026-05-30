de64c4f8aa93be9756e27d0eda1f455e68a29fe0
Lemuel <bullalemy1@gmail.com>
2026-05-30 16:50:23 +0000

    Add function to print initials in block letters
    Implemented a function to print initials 'LBB' in stylized large block letters.

--- initials.py
+++ initials.py
@@ -1,6 +1,33 @@
+# File: initials.py
+# Description: Print out my initials in stylized large block letters.
+# Assignment Number: 1
+#
+# Name: Lemuel Bulla
+# STUDENT ID: 2425402265
+# Email: 2425402265@live.gctu.edu.gh
+# Grader: Augustus
+#
+#On my honor, Lemuel Bulla, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    #A function  which is to print the initials and stylized large block letters for LBB.
+    print()
+    print("...LBB")
+    print()
+    print("...LLLL................BBBBBBBBB...........BBBBBBBBB.....")
+    print("...LLLL................BBBBBBBBBBB.........BBBBBBBBBBB.....")
+    print("...LLLL................BB......BBB.........BB......BBB.....")
+    print("...LLLL................BB.......BB.........BB.......BB.....")
+    print("...LLLL................BBBBBBBBBB..........BBBBBBBBBB......")
+    print("...LLLL................BBBBBBBBBB..........BBBBBBBBBB......")
+    print("...LLLL................BB.......BB.........BB.......BB.....")
+    print("...LLLL................BB......BBB.........BB......BBB.....")
+    print("...LLLLLLLLLLLL...**...BBBBBBBBBBB....**...BBBBBBBBBBB...**")
+    print("...LLLLLLLLLLLL...**...BBBBBBBBB......**...BBBBBBBBB.....**")
+    print()
+  
 
 
 
